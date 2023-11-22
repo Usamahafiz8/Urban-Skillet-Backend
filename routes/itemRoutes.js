087e911bd494440f742bbcbabe1b1d399f82a33a
Usamahafiz8 <usamahafiz8@gmail.com>
2023-11-22 11:24:16 +0500

    added on the swager MOdifer API

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -55,6 +55,29 @@
  *                 },
  *                 // Additional tax details...
  *               ]
+ * @swagger
+ * /items/modifier/{modifierID}:
+ *   get:
+ *     summary: Get the first item of each category for a given location
+ *     parameters:
+ *       - in: path
+ *         name: modifierID
+ *         required: true
+ *         description: ID of the location to filter items
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               - categoryId: 'TCSSX34YZ7DTVMPSOSEI6PTS'
+ *                 categoryName: 'Wings'
+ *                 item:
+ *                   itemId: 'T2BL4DTZCTUVUD4BCIXND46U'
+ *                   itemName: 'Burgar1'
+ * 
  */
 
 const express = require('express');
@@ -65,6 +88,6 @@ const ItemsControler = require('../controlers/ItemControler');
 router.get('/Popular/:locationId', ItemsControler.PopularItems);
 router.get('/Detail/:itemId/:locationId', ItemsControler.ItemInformation);
 router.get('/modifier/:modifierID', ItemsControler.ItemModifier);
-router.get('/tax/:taxID/:locationID', ItemsControler.ItemTax);
+
 
 module.exports = router;

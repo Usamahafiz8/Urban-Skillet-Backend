411e0c0c1b751a3c2bdaa68db77c08ddac1ac493
Usamahafiz8 <usamahafiz8@gmail.com>
2023-11-27 12:51:17 +0500

    working on the customers signup and login critera method and integration with the DB to store the important information

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
+require("dotenv").config();
+require("./db/connect");
 const swaggerUi = require('swagger-ui-express');
 const specs = require('./swaggerConfig'); 
 
@@ -12,6 +14,7 @@ const locationsRoutes = require('./routes/locationRoutes');
 const catalogRoutes = require('./routes/catalogRoutes');
 const itemsRoutes = require('./routes/itemRoutes');
 const ordresRoutes = require('./routes/orderRoutes');
+const customerRoute = require('./routes/customerRoutes');
 // const paymentRoutes = require('./routes/paymentRoute');
 
 // Middleware to parse JSON bodies
@@ -23,7 +26,9 @@ app.use('/locations', locationsRoutes);
 app.use('/catalog', catalogRoutes);
 app.use('/items', itemsRoutes);
 app.use('/orders', ordresRoutes);
+app.use('/customers', customerRoute);
 // app.use('/payments', paymentRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ models/customerSchema.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+  customerSquareId: { type: String, required: true },
+  createdAt: { type: Date, required: true },
+  updatedAt: { type: Date, required: true },
+  givenName: { type: String, required: true },
+  familyName: { type: String, default: '' },
+  emailAddress: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  companyName: { type: String, default: '' },
+  password: { type: String, required: true }, // Add the password field
+});
+
+const Customer = mongoose.model('Customer', customerSchema);
+
+module.exports = Customer;
